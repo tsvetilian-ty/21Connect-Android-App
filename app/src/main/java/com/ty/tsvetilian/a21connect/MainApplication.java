6d50bc8bc58d1befc395188b65e3b1f422d38fd7
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2018-02-12 15:08:32 +0200

    Added realm db

--- app/src/main/java/com/ty/tsvetilian/a21connect/MainApplication.java
+++ app/src/main/java/com/ty/tsvetilian/a21connect/MainApplication.java
@@ -2,5 +2,18 @@ package com.ty.tsvetilian.a21connect;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().name("connect21.realm").build();
+//        RealmConfiguration config = new RealmConfiguration.Builder().inMemory().name("connect21.realm").build();
+        Realm.setDefaultConfiguration(config);
+    }
 }
