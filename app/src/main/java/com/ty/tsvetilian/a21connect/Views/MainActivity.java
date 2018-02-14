7048da29db53371271f3932cfd10ba03361f0c1c
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2018-02-14 17:01:12 +0200

    Added packages to define the project structure

--- app/src/main/java/com/ty/tsvetilian/a21connect/Views/MainActivity.java
+++ app/src/main/java/com/ty/tsvetilian/a21connect/Views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ty.tsvetilian.a21connect;
+package com.ty.tsvetilian.a21connect.Views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ty.tsvetilian.a21connect.R;
 
 public class MainActivity extends AppCompatActivity {
 
